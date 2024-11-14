b368d086c26def0c0dd2dbb5a0f195640f47cdeb
gerry <gerrydesrian488@gmail.com>
2024-11-14 11:54:06 +0700

    ads validation

--- src/routes/ads-route.js
+++ src/routes/ads-route.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const adsController = require('../controllers/ads-controller');
 const upload = require('../utils/multer');
+const validate = require('../middlewares/validate');
+const adsValidation = require('../validations/ads-validation');
 
 const router = express.Router();
 
-router
-	.route('/upload')
-	.post(upload.single('image'), adsController.uploadBanner);
+router.route('/upload').post(
+	upload.single('image'),
+	validate(adsValidation.uploadBanner),
+
+	adsController.uploadBanner
+);
 
 module.exports = router;
